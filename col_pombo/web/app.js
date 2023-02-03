5b3ecc038f223bc573fde88047bbf5c37a20721b
Eduardo Froehlich <eduardo.froehlich@quantosobra.com.br>
2023-02-03 13:14:49 -0300

    Send Script for Players send Pombo

---
+++ col_pombo/web/app.js
@@ -0,0 +1,10 @@
+var isPolice = true;
+var isMedic = false;
+
+var backgroundColor = isPolice ? 'bg-sky-800/90' : 'bg-red-800/90'
+function teste() {
+    const teste = document.getElementsByClassName("testee");
+    console.log(teste);
+}
+
+teste();
